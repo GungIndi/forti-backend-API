2dff903e93d063def9ac3865d2264e0742330193
GungIndi <gungindikusumaputra@gmail.com>
2023-12-24 22:59:40 +0800

    set the vercel

--- forumAPI/forumAPI/settings.py
+++ forumAPI/forumAPI/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # from fortiApi.models import User
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -97,14 +100,24 @@ CORS_ALLOW_METHODS = (
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'restfullapidb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'ggndi523',
+#         'HOST': 'localhost',
+#         'PORT': 5432
+#     }
+# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'restfullapidb',
-        'USER': 'postgres',
-        'PASSWORD': 'ggndi523',
-        'HOST': 'localhost',
-        'PORT': 5432
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
@@ -148,6 +161,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 STATIC_URL = 'static/'
 
--- forumAPI/forumAPI/urls.py
+++ forumAPI/forumAPI/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/api/', permanent=False)),
     path('api/', include('fortiApi.urls'))
 ]
+
+# add at the last
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
